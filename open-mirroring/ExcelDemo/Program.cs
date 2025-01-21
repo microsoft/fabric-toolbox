de48fe4083e3355432ff8f0caf0d5c0bc7b89e5b
Mark Pryce-Maher <35062421+MarkPryceMaherMSFT@users.noreply.github.com>
2025-01-21 12:22:51 +0000

    Adding Open Mirroring for Excel

---
+++ open-mirroring/ExcelDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using ExcelDemo;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    public static AppConfig config = null;
+    static void Main(string[] args)
+    {
+        var AppConfigPath = @"C:\source\OpenMirroring\OpenMirroring\ExcelDemo\AppConfig.json";
+
+        string jsonString = File.ReadAllText(AppConfigPath);
+
+        // Deserialize the JSON file into the Config object
+        config = JsonSerializer.Deserialize<AppConfig>(jsonString);
+
+        Console.WriteLine("Open Mirroring Excel Demo");
+
+        Helper.CreateDir(config.folderToWatch);
+
+        Console.WriteLine($"Watching {config.folderToWatch}");
+
+        using FileSystemWatcher watcher = new FileSystemWatcher();
+
+        // Set the directory to monitor
+        watcher.Path = config.folderToWatch;
+
+        // Optionally, filter for specific files or extensions (e.g., "*.txt" for text files)
+        watcher.Filters.Add("*.xlsx");
+
+        // Watch for specific changes (created, changed, deleted, renamed)
+        watcher.NotifyFilter = NotifyFilters.FileName |   // File name changes
+                               NotifyFilters.LastWrite |  // File modifications
+                               NotifyFilters.Size |
+                               NotifyFilters.CreationTime |
+                               NotifyFilters.Attributes;        // File size changes
+
+        watcher.Created += OnCreated;
+        watcher.Changed += OnCreated;
+        watcher.Renamed += OnCreated;
+
+        watcher.EnableRaisingEvents = true;
+
+        Console.ReadLine();
+    }
+    private static void OnCreated(object sender, FileSystemEventArgs e)
+    {
+        ImportFile(e.FullPath);
+    }
+
+    private static void ImportFile(string FullPathtoExcelDocument)
+    {
+        Thread.Sleep(500);
+
+        string fileExtension = Path.GetExtension(FullPathtoExcelDocument).ToLower();
+
+        Console.WriteLine($"Importing: {FullPathtoExcelDocument}");
+        Console.WriteLine($"fileExtension: {fileExtension}");
+
+        if (FullPathtoExcelDocument.Contains("~")) // ignore temp files
+        {
+            return;
+        }
+
+        if (fileExtension == ".xlsx")
+            Excel.ConvertExcelToDatatable(FullPathtoExcelDocument, config);
+    }
+
+
+}
--- open-mirroring/README.md
+++ open-mirroring/README.md
@@ -2,5 +2,28 @@
 
 This is a collection of Opening Mirroring solutions. 
 
-SQLServerChangeTracking - Is the On-Prem SQL Server Open Mirroring solution for SQL Server 2008 and above (althought I have only tested SQL Server 2017 on Docker.)   This solution is designed to show you how you can simply and quickly setup Mirroring from virtually any SQL Server by using change tracking.
+This is a collection of open sources demos/proof of concepts for Mirroring different sources using Open Mirroing.
+
+
+They show how easily you can setup Open Mirroring for yourself.
+This is POC level code, its not for production usage.
+
+Part 1 - [Introduction to Open Mirroring in Fabric](https://youtu.be/q_ZVJ3XMH7I)
+
+Part 2 - [Create a Mirrored database](https://youtu.be/tiHHw2Hj848)
+
+Part 3 - [Setting Up Permissions for Excel Mirroring in Microsoft Fabric](https://youtu.be/85xWqWHfWbU)
+
+
+
+## SQLServerChangeTracking
+
+Is the On-Prem SQL Server Open Mirroring solution for SQL Server 2008 and above (althought I have only tested SQL Server 2017 on Docker.)   This solution is designed to show you how you can simply and quickly setup Mirroring from virtually any SQL Server by using change tracking.
+
+## Excel Mirroring
+Project for Mirroring Excel documents to Fabric.
+
+
+
+
 
