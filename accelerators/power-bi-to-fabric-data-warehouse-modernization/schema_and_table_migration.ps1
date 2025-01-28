3320ce97cca4db4d564fd0ecf43d5f18ac086485
Nick Salch <nick.salch@outlook.com>
2025-01-28 15:18:21 -0800

    powerbi modernization accelerator

---
+++ accelerators/power-bi-to-fabric-data-warehouse-modernization/schema_and_table_migration.ps1
@@ -0,0 +1,89 @@
+############################################ RUN SCRIPT TO BEGIN INPUTS
+
+# Define the datamart connection parameters
+$datamartServerAddress = Read-Host("Please provide your datamart server address ")
+$datamartDatabaseName = Read-Host("`nPleas provide your datamart name ")
+
+# Define the warehouse connection parameters
+$warehouseServerAddress = Read-Host("`nPlease provide your datawarehouse server address ")
+$warehouseDatabaseName = Read-Host("`nPlease provide your datawarehouse name ")
+
+# Define the schema you want to create in the warehouse
+do {
+    $schemaName = Read-Host("`nPlease provide the name of a new schema you want created in the warehouse ")
+    if ($schemaName -match "^(?i)dbo$") {
+        Write-Host "The name 'dbo' is not recommended. Please choose a different name."
+    }
+} while ($schemaName -match "^(?i)dbo$")
+
+# Define the tenant id you're logging into
+$tenantId = Read-Host("`nPlease provide your tenant id ")
+
+############################################ CHECK AND INSTALL MODULES
+
+# Install the necessary modules if not already installed
+if (-not (Get-Module -ListAvailable -Name Az.Accounts)) {
+    Install-Module -Name Az.Accounts -AllowClobber -Force
+    Import-Module -Name Az.Accounts
+}
+if (-not (Get-Module -ListAvailable -Name SqlServer)) {
+    Install-Module -Name SqlServer -AllowClobber -Force
+    Import-Module -Name SqlServer
+}
+
+############################################ BEGIN MIGRATION
+
+# Login with your Entra ID account
+Connect-AzAccount -TenantId $tenantId
+
+# Get the access token for the Azure SQL Database
+$accessToken = (Get-AzAccessToken -ResourceUrl 'https://database.windows.net/').Token
+
+# Define the SQL query to generate the schema and table creation script
+$sqlQuery = @"
+DECLARE @sql VARCHAR(MAX) = '',
+        @schema VARCHAR(MAX) = '$schemaName';
+
+SELECT @sql = @sql + 'CREATE TABLE ['  + @schema + '].' + TABLE_NAME + ' (' + CHAR(13) + CHAR(10) +
+    STRING_AGG(
+        COLUMN_NAME + ' ' + 
+        CASE 
+            WHEN DATA_TYPE = 'nvarchar' THEN 'varchar'
+            WHEN DATA_TYPE = 'nchar' THEN 'char'
+            WHEN DATA_TYPE = 'time' THEN 'TIME (6)'
+            WHEN DATA_TYPE = 'money' then 'DECIMAL (19,4)'
+            WHEN DATA_TYPE IN ('datetime', 'datetime2', 'smalldatetime', 'datetime2') THEN 'DATETIME2 (6)'
+            WHEN DATA_TYPE IN ('text', 'ntext') THEN 'varchar(8000)'
+            ELSE DATA_TYPE
+        END +
+        CASE
+            WHEN DATA_TYPE IN ('float', 'money', 'text', 'ntext') THEN ''
+            WHEN CHARACTER_MAXIMUM_LENGTH IS NOT NULL AND DATA_TYPE NOT IN ('bigint', 'datetime', 'date', 'time', 'datetime2', 'smalldatetime') THEN '(' + CAST(CHARACTER_MAXIMUM_LENGTH AS VARCHAR) + ')'
+            WHEN NUMERIC_PRECISION IS NOT NULL AND DATA_TYPE NOT IN ('bigint') THEN '(' + CAST(NUMERIC_PRECISION AS VARCHAR) + ',' + CAST(NUMERIC_SCALE AS VARCHAR) + ')'
+            ELSE ''
+        END, 
+        ',' + CHAR(13) + CHAR(10)
+    ) + CHAR(13) + CHAR(10) + ');' + CHAR(13) + CHAR(10)
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'model'
+GROUP BY TABLE_NAME;
+
+SELECT 'CREATE SCHEMA [' + @schema + '];
+GO
+' + @sql;
+"@
+
+# Generate the T-SQL command for schema and table creation from the existing datamart
+try {
+    $createSchema = Invoke-Sqlcmd -ServerInstance $datamartServerAddress -Database $datamartDatabaseName -AccessToken $accessToken -Query $sqlQuery
+} catch {
+    Write-Error "Failed to generate schema creation script: $_"
+}
+
+# Create a new schema and tables in the fabric data warehouse
+try {
+    Invoke-Sqlcmd -ServerInstance $warehouseServerAddress -Database $warehouseDatabaseName -AccessToken $accessToken -Query $createSchema[0]
+    Write-Output "Schema and tables created successfully in the fabric warehouse."
+} catch {
+    Write-Error "Failed to create schema and tables in the fabric warehouse: $_"
+}
--- accelerators/real-time-intelligence_eventstream/README.md
+++ accelerators/real-time-intelligence_eventstream/README.md
@@ -16,38 +16,6 @@ Jump to the relavent section by using the links below
 * [Eventhouse](https://github.com/SuryaTejJosyula/FabricRTI_Accelerator#eventhouse)
 * [Eventstream](https://github.com/SuryaTejJosyula/FabricRTI_Accelerator#eventstream)
 
-# Eventhouse
-
-[Eventhouse and KQL DB APIs Blog](https://blog.fabric.microsoft.com/en-us/blog/using-apis-with-fabric-real-time-analytics/)
-## Instructions
-To create Eventhouse and its entities, run the PowerShell script with following parameters
-
-|Parameter|Mandatory?|Description|
-|--------|--------|----------|
-|tenantId|Yes|Tenant ID of your Fabric tenant|
-|workspaceId|	Yes	|Workspace ID where you want to create Eventhouse|
-|eventhouseName |	Yes|	Display name of your Eventhouse|
-|kqlDBName |	No|	Name of your KQL DB. If not provided, default KQL DB in Eventhouse will be used|
-|dbScriptName |	No|	Database script with creation script of all entities|
-
-* Make sure you have Azure modules installed in Powershell.
- ```
-	Install-Module Az 
-```
-* Make sure you have the database script in the same folder as the Powershell script. A test database script is provided here - TestPS_KQL.csl . This script creates a simple table testPS3 and a function GetNames()._
-* The script will open an interactive login window and then ask for a selection of tenant and subscription (Yes! You must select subscription although you are working with Fabric). You can set any default subscription for subsequent runs 
-```
-Update-AzConfig -DefaultSubscriptionForLogin "My_Subscription_Name" 
-```
-* Run the script
-```
-.\createEventhouse.ps1 -tenantId 'xxxxxx-yyyy-aaaa-bbbb-aaaaaaaaa'  -workspaceId 'xxxxx-yyyy-yyyy-aaaa-aaaaaaaaaaaa' -eventhouseName 'Eventhouse_001' -kqlDBName 'KQLDB_001' -dbScriptName 'CreateDB_script.csl' 
-```
-
-* Example of Eventhouse, KQL Database, table and a function created using this Powershell script
-  
-  ![Eventhouse](./media/Created_Entities.png)
-
 # Eventstream
 
 [Eventstream APIs Docs](https://learn.microsoft.com/en-us/fabric/real-time-intelligence/event-streams/eventstream-rest-api#create-eventstream-item-with-definition)
@@ -74,5 +42,5 @@ Please follow the steps below in order to create the right payload for Eventstre
 
 * Example of Eventstream created along with defined topology
 
-  ![Eventstream](./media/Eventstream_Created.png)
+  ![Eventstream](../media/Eventstream_Created.png)
 
