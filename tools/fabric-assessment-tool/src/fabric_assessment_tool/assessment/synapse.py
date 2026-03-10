a7b959060a102d56e144a94d56fe700b4e0506f9
Jorge Docampo <jorge.docampo@outlook.com>
2026-03-10 18:42:31 +0100

    Add Spark Configuration component

--- tools/fabric-assessment-tool/src/fabric_assessment_tool/assessment/synapse.py
+++ tools/fabric-assessment-tool/src/fabric_assessment_tool/assessment/synapse.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
 
 from .common import AssessmentStatus
 
@@ -360,6 +360,28 @@ class SynapseLibrary:
 
 
 @dataclass
+class SynapseSparkConfiguration:
+    """Synapse Spark Configuration information."""
+
+    name: str
+    description: str
+    configs: Dict[str, str]
+    created: str
+    created_by: str
+    source_pool: str
+    notebook_refs: int
+    sjd_refs: int
+    json_response: Any
+
+
+@dataclass
+class SynapseSparkConfigurations:
+    """Collection of Spark Configurations in a Synapse workspace."""
+
+    spark_configurations: List[SynapseSparkConfiguration]
+
+
+@dataclass
 class SynapseLibraries:
     """Collection of Libraries in a Synapse workspace."""
 
@@ -385,6 +407,7 @@ class SynapseAssessment:
     datasets: SynapseDatasets
     managed_private_endpoints: SynapseManagedPrivateEndpoints
     libraries: SynapseLibraries
+    spark_configurations: SynapseSparkConfigurations
 
     assessment_metadata: SynapseAssessmentMetadata
 
